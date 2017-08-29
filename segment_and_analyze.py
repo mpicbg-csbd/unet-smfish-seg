Predict bugfix. segtools update. unet normalization fix. remote_summary shell script.

segment_and_analyze.py
@@ -1,7 +1,7 @@
 import skimage.io as io
 import numpy as np
 from scipy.ndimage import label
-import label_imgs
+import segtools
 import util
 
 import matplotlib.pyplot as plt
@@ -11,7 +11,7 @@ def upscale_and_compare(labeling, annotated):
     a,b = labeling.shape
     _,c,d = annotated.shape
     upscaled = zoom(labeling, (c/a, d/b), order=0)
-    score = label_imgs.match_score_1(annotated[0], upscaled)
+    score = segtools.match_score_1(annotated[0], upscaled)
     io.imsave('upscaled.tif', upscaled)
     io.imsave('cells.tif', annotated[0])
     return score
@@ -23,8 +23,18 @@ def compare_segment_predictions_with_groundtruth(segs, labels):
         simg = io.imread(s)
         limg = io.imread(l)
         print('\n', s)
-        return label_imgs.match_score_1(simg, limg)
+        return segtools.match_score_1(simg, limg)
     return map(print_and_score, zip(segs, labels))
+
+
+def compare_labs_ypreds(labs, ypreds):
+    for lab, ypr in zip(labs, ypreds):
+        lab = io.imread(lab)
+        lab[lab==2] = 1 # membrane images
+        ypr = io.imread(ypr)
+        ypr_labeled = get_label(ypr, 0.5)
+        mat = segtools.pixel_sharing_graph(lab, ypr_labeled)
+
 
 def get_label(img, threshold):
     "normalizes img min&max to [0,1), then binarize at threshold, then labels connected components."
@@ -54,7 +64,7 @@ def segment_classified_images(membranes, threshold):
     for fname, img in zip(membranes, res):
         path, base, ext = util.path_base_ext(fname)
         io.imsave(base + '_seg' + ext, img) 
-        io.imsave(base + '_seg_preview' + ext, label_imgs.labelImg_to_rgb(img))
+        io.imsave(base + '_seg_preview' + ext, segtools.labelImg_to_rgb(img))
     return res
 
 def combine_into_readme_img(greys, mems, segs):

segtools.py
@@ -38,7 +38,6 @@ def pastel_colors_RGB_gap(n_colors=10, brightness=0.5, value=0.5):
 def label_colors(bg_ID=1, membrane_ID=0, n_colors = 10, maxlabel=1000):
     RGB_tuples = pastel_colors_RGB(n_colors=10)
     # intens *= 2**16/intens.max()
-
     assert membrane_ID != bg_ID
     RGB_tuples *= maxlabel
     RGB_tuples[membrane_ID] = (0, 0, 0)
@@ -66,17 +65,21 @@ def labelImg_to_rgb(img, bg_ID=1, membrane_ID=0):
 # MISC
 
 @jit
-def permute_img(img, perm):
+def permute_img(img, perm=None):
     """
     Permute the labels on a labeled image according to `perm`, if `perm` not given
     then permute them randomly.
     Returns a copy of `img`.
     """
+    print("ok")
+    if not perm:
+        perm = np.arange(img.max()+1)
+        np.random.shuffle(perm)
     res = img.copy()
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            img[i,j] = perm[img[i,j]]
-    return img
+            res[i,j] = perm[img[i,j]]
+    return res
 
 def permutation_from_matching(matching):
     ar = np.arange(matching.shape[0])
@@ -113,7 +116,7 @@ def pixel_sharing_graph(img1, img2):
 
 def matching_overlap(mat, fraction=0.5):
     """
-    create a matching given two label images based on mutually overlapping regions of sufficient size.
+    create a matching given pixel_sharing_graph of two label images based on mutually overlapping regions of sufficient size.
     NOTE: a true matching is only gauranteed for fraction > 0.5. Otherwise some cells might have deg=2 or more.
     NOTE: doesn't break when the fraction of pixels matching is a ratio only slightly great than 0.5? (but rounds to 0.5 with float64?)
     """

unet.py
@@ -31,7 +31,7 @@ def normalize_X(X):
     X -= mi
     #ma = np.amax(X,axis = (1,2), keepdims = True) + 1.e-10
     #ma = np.percentile(X, 99, axis = (1,2), keepdims = True) + 1.e-10
-    ma = np.percentile(X, 99) + 1.e-10
+    ma = np.percentile(X, 100) + 1.e-10
     X /= ma
     X = np.clip(X, 0, 1)
     return X